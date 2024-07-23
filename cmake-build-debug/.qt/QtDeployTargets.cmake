09e6fbf237d46a5f26759e612b3575f55a03ac53
scottXchoo <ckh0601@cau.ac.kr>
2024-07-23 19:18:43 +0900

    fix : remove .gitignore

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_QML_Project_Template_FILE /Users/chooble/Documents/Qt-Study/CLion-QtQuick-QML-Template/cmake-build-debug/QML_Project_Template)
+set(__QT_DEPLOY_TARGET_QML_Project_Template_TYPE EXECUTABLE)
